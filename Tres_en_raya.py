4dca01bd66de46747b28609d19ff8f9e47aeba57
Domingo Canosa <domicc5@hotmail.com>
2023-03-28 22:29:09 +0200

    Cambiando definiciones. Sin acabar

--- Tres_en_raya.py
+++ Tres_en_raya.py
@@ -1,22 +1,23 @@
-#VARIBALES DEL JUEGO
-
+#Importación de los módulos y funciones necesarias
 from IPython.display import clear_output
-from modulos import definiciones as df
+from modulos import definiciones as d
 
-    #generales
-game_on = True
-comienzo_juego = True
-ganador = False
-    #jugadores
-posicion = ''
-turno = 'Jugador 1 (X)'
-jugador1 = 'X'
-jugador2 = 'O'
-    #posiciones del tablero
-tablero = {'1':' ','2':' ','3':' ','4':' ','5':' ','6':' ','7':' ','8':' ','9':' ',}
+#Estableciendo la lógica del juego para retocar las funciones.
+#Las funciones no pueden depender de otras, tienen que ser totalmente independientes.
+#De forma que al ejecutarlas la lógica del juego sea mucho más clara y no interfieran entre ellas.
 
-while game_on:
-    df.display()
-    posicion = df.pos_validar()
-    df.pos_tablero()
-    df.verificacion_final()
+#Inicio del juego. Se establecen las variables principales
+d.inicio_variables()
+
+#Mensaje de bienvenida al juego.
+print('\r\nBIENVENIDOS AL JUEGO DEL TRES EN RAYA')
+print('Versión de juego para jugar en la terminal de Windows')
+print('Siempre comienza primero el Jugador 1 (X), Suerte !! \r\n')
+
+
+
+#while df.game_on:
+#    df.display()
+#    posicion = df.pos_validar()
+#    df.pos_tablero()
+#    df.verificacion_final()
--- modulos/definiciones.py
+++ modulos/definiciones.py
@@ -1,23 +1,43 @@
 
 from IPython.display import clear_output
 
-    #generales
-game_on = True
-comienzo_juego = True
-ganador = False
-    #jugadores
-posicion = ''
-turno = 'Jugador 1 (X)'
-jugador1 = 'X'
-jugador2 = 'O'
-    #posiciones del tablero
-tablero = {'1':' ','2':' ','3':' ','4':' ','5':' ','6':' ','7':' ','8':' ','9':' ',}
+def inicio_variables():
+    """
+    Función que establece el valor inicial de las variables generales del juego. Válido para primera ejecución o como para reinicio del juego.
+    """
+    #Primero, se hacen las variables globales
+    global game_on
+    global comienzo_juego
+    global ganador
+    global empate
+    global posicion
+    global turno
+    global jugador1
+    global jugador2
+    global tablero
 
-def board():
-    """
-    Función que muestra el tablero de juego
-    """
+    #Segundo, se establecen los valores iniciales de las variables
+    #Variables del juego
+    game_on = True
+    comienzo_juego = True
+    ganador = False
+    empate = False
     
+    #Variables de jugadores
+    posicion = ''
+    turno = 'Jugador 1 (X)'
+    jugador1 = 'X'
+    jugador2 = 'O'
+    
+    #Variables de posiciones del tablero. Se establece un diccionario con un valor asignado a cada posición.
+    tablero = {'1':' ','2':' ','3':' ','4':' ','5':' ','6':' ','7':' ','8':' ','9':' ',}
+
+def display_board():
+    """
+    Función que muestra el estado actual del tablero.
+    """
+    global tablero
+
     print(' ___________')
     print('|   |   |   |')
     print(f"| {tablero['7']} | {tablero['8']} | {tablero['9']} |")
@@ -27,139 +47,104 @@ def board():
     print('|___|___|___|')
     print('|   |   |   |')
     print(f"| {tablero['1']} | {tablero['2']} | {tablero['3']} |")
-    print('|___|___|___|')
+    print('|___|___|___|\r\n')
 
 def cambio_turno():
     """
-    Función que cambia el turno de un jugador al otro
+    Función que cambia el turno de Jugador 1 a Jugador 2 y viceversa.
     """
-    
     global turno
-    
+
     if turno == 'Jugador 1 (X)':
-                
         turno = 'Jugador 2 (O)'
     
     else:
-        
         turno = 'Jugador 1 (X)'
 
-def validar_tablero():
+def pregunta_posicion():
     """
-    Función que verifica el tablero, buscando el estado de la partida:
-        - No hay ganador
-        - Hay ganador
+    Pregunta al jugador la posición que elige y verifica que sea una respuesta válida.
+    Una vez la posicion escogida es correcta, la asigna al diccionario 'tablero'.
+    Válido tanto para Jugador 1 como para Jugador 2.
     """
-    
-    global ganador
-    
-    if (tablero['1'] == 'X' and tablero['2'] == 'X' and tablero['3'] == 'X') == True:
-        ganador = True
-    if (tablero['4'] == 'X' and tablero['5'] == 'X' and tablero['6'] == 'X') == True:
-        ganador = True
-    if (tablero['7'] == 'X' and tablero['8'] == 'X' and tablero['9'] == 'X') == True:
-        ganador = True
-    if (tablero['1'] == 'X' and tablero['4'] == 'X' and tablero['7'] == 'X') == True:
-        ganador = True
-    if (tablero['2'] == 'X' and tablero['5'] == 'X' and tablero['8'] == 'X') == True:
-        ganador = True
-    if (tablero['3'] == 'X' and tablero['6'] == 'X' and tablero['9'] == 'X') == True:
-        ganador = True
-    if (tablero['1'] == 'X' and tablero['5'] == 'X' and tablero['9'] == 'X') == True:
-        ganador = True
-    if (tablero['3'] == 'X' and tablero['5'] == 'X' and tablero['7'] == 'X') == True:
-        ganador = True
+    while True:
         
-    if (tablero['1'] == 'O' and tablero['2'] == 'O' and tablero['3'] == 'O') == True:
-        ganador = True
-    if (tablero['4'] == 'O' and tablero['5'] == 'O' and tablero['6'] == 'O') == True:
-        ganador = True
-    if (tablero['7'] == 'O' and tablero['8'] == 'O' and tablero['9'] == 'O') == True:
-        ganador = True
-    if (tablero['1'] == 'O' and tablero['4'] == 'O' and tablero['7'] == 'O') == True:
-        ganador = True
-    if (tablero['2'] == 'O' and tablero['5'] == 'O' and tablero['8'] == 'O') == True:
-        ganador = True
-    if (tablero['3'] == 'O' and tablero['6'] == 'O' and tablero['9'] == 'O') == True:
-        ganador = True
-    if (tablero['1'] == 'O' and tablero['5'] == 'O' and tablero['9'] == 'O') == True:
-        ganador = True
-    if (tablero['3'] == 'O' and tablero['5'] == 'O' and tablero['7'] == 'O') == True:
-        ganador = True
+        global posicion
 
-def pos_validar():
-    """
-    Función para validar la posición elegida por el jugador
-    """
-    
-    eleccion = 'wrong'
-    en_rango = False
-    
-    while eleccion.isdigit() == False or en_rango == False:
+        #Se pregunta al jugador que casilla quiere marcar
+        posicion = input('Elige tu próxima casilla (1-9): ') 
         
-        eleccion = input('Elige tu próxima casilla (1-9): ') 
-        
-        if eleccion.isdigit() == False:
-            
+        #Se comprueba que el valor introducido es un número y no un string
+        if posicion.isdigit() == False:
             #clear_output()
             print('No has elegido un valor correcto. Elige un número entre el 1 y el 9')
             
-        if eleccion.isdigit() == True:
-            
-            if int(eleccion) in range(1,10):
+        else:
+            if int(posicion) in range(1,10):
                 #clear_output()
-                en_rango = True
+
+                #Se comprueba que la posición escogida no está ocupada
+                if tablero[posicion] != ' ':
+                    print('Esta posición está ocupada, elige otra!')
+                    continue
+                else:
+                    if turno == 'Jugador 1 (X)':
+                        tablero[posicion] = 'X'
+                        break
+                    else:
+                        tablero[posicion] = 'O'
+                        break
             else:
                 #clear_output()
                 print('No has elegido un valor correcto. Elige un número entre el 1 y el 9')
-                en_rango = False
-                
-    return eleccion
 
-def pos_tablero():
+def buscar_resultado():
     """
-    Función que valida y asigna la posición elegida por el jugador en el tablero
+    Función que verifica el tablero, buscando el estado de la partida entre estos casos:
+        - Hay un ganador
+        - La partida sigue (se produce cambio de turno)
+        - Hay un empate
     """
+    global ganador
+    global empate
+
+    #Con esta serie de if's, se comprueba si algún jugador ha ganado
+    if (tablero['1'] == tablero['2'] == tablero['3'] == 'X') or (tablero['1'] == tablero['2'] == tablero['3'] == 'O'):
+        ganador = True
+    if (tablero['4'] == tablero['5'] == tablero['6'] == 'X') or (tablero['4'] == tablero['5'] == tablero['6'] == 'O'):
+        ganador = True
+    if (tablero['7'] == tablero['8'] == tablero['9'] == 'X') or (tablero['7'] == tablero['8'] == tablero['9'] == 'O'):
+        ganador = True
+    if (tablero['1'] == tablero['4'] == tablero['7'] == 'X') or (tablero['1'] == tablero['4'] == tablero['7'] == 'O'):
+        ganador = True
+    if (tablero['2'] == tablero['5'] == tablero['8'] == 'X') or (tablero['2'] == tablero['5'] == tablero['8'] == 'O'):
+        ganador = True
+    if (tablero['3'] == tablero['6'] == tablero['9'] == 'X') or (tablero['3'] == tablero['6'] == tablero['9'] == 'O'):
+        ganador = True
+    if (tablero['1'] == tablero['5'] == tablero['9'] == 'X') or (tablero['1'] == tablero['5'] == tablero['9'] == 'O'):
+        ganador = True
+    if (tablero['3'] == tablero['5'] == tablero['7'] == 'X') or (tablero['3'] == tablero['5'] == tablero['7'] == 'O'):
+        ganador = True
     
-    global posicion
-    
-    while tablero[posicion] != ' ':
-        
-        print('Esta posición está ocupada, elige otra!')
-        
-        posicion = pos_validar()
-    
-    if turno == 'Jugador 1 (X)':
-    
-        tablero[posicion] = 'X'
+    #Si hay ganador se da mensaje de victoria
+    if ganador == True:
+        print("\r\n---------------------------------")
+        print(f"EL {turno} HA GANADO LA PARTIDA !!")
+        print("---------------------------------\r\n")
     
     else:
-        
-        tablero[posicion] = 'O'
-
-def display():
-    """
-    Función que muestra el tablero en la pantalla (modificar esta función ya que está duplicada)
-    """
-    
-    global tablero
-    global comienzo_juego
-    global game_on
-    global turno
-    
-    clear_output()
-    
-    if comienzo_juego == True:
-        
-        turno = 'Jugador 1 (X)'
-        tablero = {'1':' ','2':' ','3':' ','4':' ','5':' ','6':' ','7':' ','8':' ','9':' ',}
-        print('Bienvenidos al 3 en Raya !!')
-        print('Poned vuestras mentes a punto y que gane el mejor')
-        print('Siempre comienza primero el Jugador 1 (X), Suerte !! \r\n')
-        comienzo_juego = False
-    
-    print(f'Turno de {turno}')
-    board()
+        #Se busca si queda algún movimiento por hacer, si no queda ninguno, la partida acabó en empate
+        for x in tablero.values():
+            if x == ' ':
+                break
+            else:
+                empate = True
+                print("\r\n---------------------------------")
+                print(f"LA PARTIDA HA ACABADO EN EMPATE !!")
+                print("---------------------------------\r\n")
+                
+        empate = False
+        cambio_turno()
 
 def verificacion_final():
     """
@@ -172,18 +157,16 @@ def verificacion_final():
     empate = True
     tablero_lleno = False
     
-    validar_tablero()
+    buscar_resultado()
     
     if ganador == False:
         
         for x in tablero.values():
             
             if x ==  ' ':
-                
                 empate = False
             
             else:
-                
                 tablero_lleno = True            
             
         cambio_turno()
@@ -192,7 +175,6 @@ def verificacion_final():
         
         clear_output()
         print(f'HA GANADO EL {turno}, ENHORABUENA !!')
-        board()
         print(f'HA GANADO EL {turno}, ENHORABUENA !!\r\n')
         
         volver_jugar()
@@ -201,7 +183,6 @@ def verificacion_final():
         
         clear_output()
         print(f'LA PARTIDA HA ACABADO EN EMPATE !!')
-        board()
         print(f'LA PARTIDA HA ACABADO EN EMPATE !!\r\n')
         
         volver_jugar()
@@ -225,7 +206,6 @@ def volver_jugar():
     if (respuesta == 'n') == True:
             
         clear_output()
-        board()
         print('Hasta la próxima!!')
         game_on = False
         comienzo_juego = True
