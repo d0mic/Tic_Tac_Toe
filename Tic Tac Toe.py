3ad0f1569101449ca3af6178e06e99a12242695c
Domingo Canosa <domicc5@hotmail.com>
2023-03-29 12:03:21 +0200

    Changed game code to English

---
+++ Tic Tac Toe.py
@@ -0,0 +1,19 @@
+#Modules and packages
+from IPython.display import clear_output
+from modulos import Functions as d
+
+#Start of the game. Initializing all the variables.
+d.start_variables()
+
+#Welcome message
+print('\r\nWELCOME TO THE TIC TAC TOE GAME')
+print('VERSION TO PLAY IN THE WINDOWS TERMINAL WITH THE TRACKPAD (NUMBERS FROM 1 TO 9)')
+print('The Player 1 (X) always starts, Good Luck !! \r\n')
+
+
+
+#while df.game_on:
+#    df.display()
+#    posicion = df.pos_validar()
+#    df.pos_tablero()
+#    df.verificacion_final()
